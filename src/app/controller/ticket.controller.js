fe2010c59f351580051b94b92fe5ff69388ff529
Nathan Rogan <nathan.rogan@wmca.org.uk>
2021-05-07 12:46:07 +0100

    fix doubble image displaying in lightwindow

--- src/app/controller/ticket.controller.js
+++ src/app/controller/ticket.controller.js
@@ -19,7 +19,8 @@
     '$scope',
     '$timeout',
     'deviceDetector',
-    '$location'
+    '$location',
+    '$window'
   ];
 
   function TicketDetailCtrl(
@@ -30,7 +31,8 @@
     $scope,
     $timeout,
     deviceDetector,
-    $location
+    $location,
+    $window
   ) {
     const vm = this;
     vm.loadingText = 'Loading...'; // default loading text
@@ -66,6 +68,7 @@
     vm.toggleModalTrain = toggleModalTrain;
     vm.toggleModalSwift = toggleModalSwift;
     vm.toggleModalGPay = toggleModalGPay;
+    vm.widthView = widthView; // Check width of page
     vm.operatorList = []; // Define Operator list
     vm.limit = 4; // Set paging limit for Alt tickets
     vm.deviceDetect = deviceDetect; // Function to detect device
@@ -194,6 +197,29 @@
     function toggleModalGPay() {
       vm.modalShownGpay = !vm.modalShownGpay;
     }
+
+    // check resolution so the where can i use view is not shown twice
+    vm.includeDesktopTemplate = false;
+    vm.includeMobileTemplate = false;
+    widthView();
+    function widthView() {
+      const screenWidth = $window.innerWidth;
+      console.log('width function');
+
+      if (screenWidth < 890) {
+        vm.includeMobileTemplate = true;
+        vm.includeDesktopTemplate = false;
+      } else {
+        vm.includeMobileTemplate = false;
+        vm.includeDesktopTemplate = true;
+      }
+    }
+
+    // ensure where can i use view is reloaded when page resolution goes below 890px
+    const appWindow = angular.element($window);
+    appWindow.bind('resize', function() {
+      widthView();
+    });
   }
 
   // FILTERS
