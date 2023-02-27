2e71bfc503bc7f278a0b2398ac8dba8bdaf9d754
Nathan Rogan <rolorogan@gmail.com>
2023-02-27 21:59:02 +0000

    rebuild staging version

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ const fs = require("fs");
 
 const json = JSON.parse(fs.readFileSync("./package.json"));
 
-let build = "live";
+let build = "azure";
 // Function that is ran when buildAll is called to determine buildEnv
 // This matches the buildDirs in package.json
 function determineBuild(done) {
@@ -40,7 +40,7 @@ function determineBuild(done) {
       build = "azure";
       break;
     default:
-      build = "live";
+      build = "azure";
       break;
   }
   done();
